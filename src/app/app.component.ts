7b5f145c467adfbeec1c5193c489afbf860787a3
Judson Terrell <judsonterrell@Judsons-Mac-Pro.local>
2018-02-12 19:16:09 -0500

    latest

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,8 @@
+
 import { Component, ViewChild } from '@angular/core';
 import { Nav, Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
-
-import { HomePage } from '../pages/home/home';
-import { ListPage } from '../pages/list/list';
 
 @Component({
   templateUrl: 'app.html'
@@ -12,7 +10,7 @@ import { ListPage } from '../pages/list/list';
 export class MyApp {
   @ViewChild(Nav) nav: Nav;
 
-  rootPage: any = HomePage;
+  rootPage: string = "home";
 
   pages: Array<{title: string, component: any}>;
 
@@ -21,8 +19,11 @@ export class MyApp {
 
     // used for an example of ngFor and navigation
     this.pages = [
-      { title: 'Home', component: HomePage },
-      { title: 'List', component: ListPage }
+      { title: 'Home', component: 'home' },
+      { title: 'About', component: 'about' },
+      { title: 'Products', component: 'products' },
+      { title: 'Contact', component: 'contact' },
+      { title: 'Policies', component: 'policies' }
     ];
 
   }
